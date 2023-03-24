aa3c8575bb6210272524ea8402a4bb5b682deacb
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-03-24 10:37:06 +0700

    Membuat Prosedur Input

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -10,8 +10,25 @@ void input() {          // Procedure input
         cout << "Masukkan Jumlah Data Pada Array : ";   // Membuat inputan jumlah elemen array
         cin >> n;                                       // Memanggil variabel inputan n
 
+        if (n <= 20) {                                  // Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukan maksimal 20 Elemen.\n";  // Menampilkan pesan jika data lebih dari 20
+        }
     }
+    cout << endl;                                       // Membuat jarak perbaris program
+    cout << "======================" << endl;           // Membuat tampilan susunan data elemen array
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
 
+
+    for (int i = 0; i < n; i++)                 // Menggunakan perulangan for untuk menyimpan data pada array
+    {
+        cout << "Data ke - " << (i + 1) << ": ";    // Memasukkan atau menginputkan nilai data n
+        cin >> arr[i];                              // Menyimpan nilai data n kedalam array arr
+    }
 }  
 
 int main()
